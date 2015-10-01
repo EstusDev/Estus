87c1b8fa59f9b1bab3101290464b5fb8de916af5
EstusDev <estusdev@gmail.com>
2015-09-30 23:53:38 -0700

    added EvalStackTest

---
+++ estus-optimization/src/test/scala/EvalStackTest.scala
@@ -0,0 +1,44 @@
+package com.estus.optimization
+
+import org.scalatest.{Matchers, FlatSpec}
+
+
+
+class EvalStackTest extends FlatSpec with Matchers {
+
+  "An EvalStack" should
+    "be able to peak the stack" in {
+    val evalStack = EvalStack[String, Int]()
+    evalStack.push(("id", 1))
+    evalStack.size should be (1)
+    evalStack.peak() should be (Some(("id", 1)))
+  }
+
+  it should
+    "be able to push a node" in {
+    val evalStack = EvalStack[String, Int]()
+    evalStack.push(("id", 1))
+    evalStack.size should be (1)
+    evalStack.peak() should be (Some(("id", 1)))
+  }
+
+  it should
+    "be able to push a node when there is size restriction" in {
+    val evalStack = EvalStack[String, Int]()
+    evalStack.push(("id1", 1), Some(2))
+    evalStack.push(("id2", 2), Some(2))
+    evalStack.push(("id3", 3), Some(2))
+    evalStack.size should be (2)
+    evalStack.peak() should be (Some(("id3", 3)))
+  }
+
+  it should
+    "be able to pop a node" in {
+    val evalStack = EvalStack[String, Int]()
+    evalStack.push(("id", 1))
+    evalStack.size should be (1)
+    evalStack.pop() should be (Some(("id", 1)))
+    evalStack.size should be (0)
+  }
+
+}
